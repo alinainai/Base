6e6618ca668d75f05a90f874637905cfa99c8647
lijiaxing <lijiaxing@360.cn>
2020-06-17 11:13:02 +0800

    feat: code format

--- app/src/main/java/com/gas/app/config/AppLifecyclesImpl.java
+++ app/src/main/java/com/gas/app/config/AppLifecyclesImpl.java
@@ -31,7 +31,7 @@ public class AppLifecyclesImpl implements AppLifecyclers {
 
     @Override
     public void attachBaseContext(@NonNull Context base) {
-//        MultiDex.install(base);  //这里比 onCreate 先执行,常用于 MultiDex 初始化,插件化框架的初始化
+
     }
 
     @Override
