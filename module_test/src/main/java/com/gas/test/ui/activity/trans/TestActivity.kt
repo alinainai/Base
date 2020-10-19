86617878075fc2682d48bdf806d5d5b74fb7c4e7
lijiaxing <lijiaxing@360.cn>
2020-10-19 11:27:42 +0800

    feat: 线形图根据UI继续优化

--- module_test/src/main/java/com/gas/test/ui/activity/trans/TestActivity.kt
+++ module_test/src/main/java/com/gas/test/ui/activity/trans/TestActivity.kt
@@ -9,6 +9,7 @@ import com.base.lib.mvp.IPresenter
 import com.gas.test.R
 import com.gas.test.utils.fragment.asynclist.AsyncListFragment
 import com.lib.commonsdk.constants.RouterHub
+import com.lib.commonsdk.kotlin.extension.app.debug
 import kotlinx.android.synthetic.main.activity_test.*
 
 @Route(path = RouterHub.TEST_HOMEACTIVITY)
@@ -26,17 +27,31 @@ class TestActivity : BaseActivity<IPresenter>() {
             FragmentContainerActivity.startActivity(this, AsyncListFragment::class.java)
         }
         btnModule2.setOnClickListener {
+
+            val list1 = mutableListOf<String>("11","12","13","14")
+            val list2 = mutableListOf<String>("11","12","13","14")
+            debug("btnModule2=${list1 == list2}")
+
         }
         btnModule3.setOnClickListener {
+
+
+
         }
         btnPlugin1.setOnClickListener {
-
+            val list1 = mutableListOf<String>("11","12","13","14")
+            val list2 = mutableListOf<String>("11","13","12","14")
+            debug("btnPlugin1=${list1 == list2}")
         }
         btnPlugin2.setOnClickListener {
-
+            val list1 = mutableListOf<String>("11","12","13")
+            val list2 = mutableListOf<String>("11","13","12","14")
+            debug("btnPlugin2=${list1 == list2}")
         }
         btnPlugin3.setOnClickListener {
-
+            val list1 = mutableListOf<String>()
+            val list2 = mutableListOf<String>()
+            debug("btnPlugin3=${list1 == list2}")
         }
     }
 }
