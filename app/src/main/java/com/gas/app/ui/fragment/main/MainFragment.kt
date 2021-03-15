0983b1a7bf052ac229be3564bdc21b68f2ccfe07
lijiaxing <lijiaxing@whale.ws>
2021-03-15 18:30:54 +0800

    feat: flutter 消息交互试水3

--- app/src/main/java/com/gas/app/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/gas/app/ui/fragment/main/MainFragment.kt
@@ -1,23 +1,12 @@
 package com.gas.app.ui.fragment.main
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.text.Html
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.AbsoluteSizeSpan
-import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.launcher.ARouter
-import com.base.baseui.dialog.ItemSelectDialog
 import com.base.componentservice.gank.service.GankInfoService
 import com.base.componentservice.test.service.TestInfoService
 import com.base.componentservice.zhihu.service.ZhihuInfoService
@@ -29,13 +18,9 @@ import com.gas.app.R
 import com.gas.app.ui.fragment.main.di.DaggerMainComponent
 import com.gas.app.ui.fragment.main.mvp.MainContract
 import com.gas.app.ui.fragment.main.mvp.MainPresenter
-import com.gas.app.utils.AppModuleUtil
 import com.gas.flutterplugin.learn.TurnToFlutterActivity
 import com.lib.commonsdk.constants.RouterHub
-import com.lib.commonsdk.extension.app.debug
 import com.lib.commonsdk.extension.app.navigation
-import com.lib.commonsdk.kotlin.utils.fromJson
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_main.*
 
 
@@ -67,30 +52,12 @@ class MainFragment : BaseFragment<MainPresenter?>(), MainContract.View {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    private val timeDownNum1 = 10L
-    private val timeDownNum2 = 10L
-    private val holderCompositeSubscription = CompositeDisposable()
-    private val compositeSubscription = CompositeDisposable().apply {
-        add(holderCompositeSubscription)
-    }
-
     override fun initData(savedInstanceState: Bundle?) {
         ARouter.getInstance().inject(this)
         loadModuleInfo()
 
         btnModule1.setOnClickListener {
-
-
-//            val str1 = getString(R.string.little_cloud_data_size, AppModuleUtil.formatMemorySizeB(53687091200), AppModuleUtil.formatMemorySizeB(12345678))
-//            tv_componentify.text = Html.fromHtml(str1);
-//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-//                tv_componentify.text = Html.fromHtml(str1, Html.FROM_HTML_MODE_LEGACY);
-//            } else {
-//                tv_componentify.text = Html.fromHtml(str1);
-//            }
-
-//            navigation(activity, RouterHub.ZHIHU_HOMEACTIVITY)
-            startActivity(Intent(context, TurnToFlutterActivity::class.java))
+            navigation(activity, RouterHub.ZHIHU_HOMEACTIVITY)
         }
         btnModule2.setOnClickListener {
             navigation(activity, RouterHub.GANK_HOMEACTIVITY)
@@ -99,61 +66,15 @@ class MainFragment : BaseFragment<MainPresenter?>(), MainContract.View {
             navigation(activity, RouterHub.TEST_HOMEACTIVITY)
         }
         btnPlugin1.setOnClickListener {
-            showBatteryOptimizeTimeDialog(context as Activity, "10") { item ->
-                debug(item.key)
-            }
+            startActivity(Intent(context, TurnToFlutterActivity::class.java))
         }
         btnPlugin2.setOnClickListener {
 
         }
         btnPlugin3.setOnClickListener {
-//            tvReportTypeViewNum1.text = textFormatStyle(521)
-          val testModel =  fromJson(jsonStr1,TestModel::class.java)
-           Log.e("testModel",testModel.description)
 
         }
 
-    }
-
-    val jsonStr = "{\"id\":1,\"description\":\"Test\"}"
-    val jsonStr1 = "{\"id\":1}"
-
-    data class TestModel(
-            val id: Int,
-            val description: String = ""
-    )
-
-    private fun textFormatStyle(num: Int): SpannableStringBuilder {
-        return if (num > 0) {
-            val startStr = num.toString()
-            val endFix = "条"
-            val str = "$startStr$endFix"
-            SpannableStringBuilder(str).apply {
-                setSpan(ForegroundColorSpan(Color.parseColor("#2B2D2F")), 0, startStr.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                setSpan(AbsoluteSizeSpan(30, true), 0, startStr.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                setSpan(ForegroundColorSpan(Color.parseColor("#7A7E8E")), startStr.length, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                setSpan(AbsoluteSizeSpan(9, true), startStr.length, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            }
-        } else {
-            SpannableStringBuilder("暂无消息")
-        }
-    }
-
-    private fun initEventTitle() {
-        llContent.removeAllViews()
-        val list = listOf("第一条Preconditions.checkNotNull(message) setData(data: Any?) setData(data: Any?)",
-                " 第二条 ArmsUtils.snackbarText(message) Preconditions.checkNotNull(intent)",
-                "第三条 ArmsUtils.startActivity(intent) Preconditions.checkNotNull(intent)",
-                "第四条 Preconditions.checkNotNull(message) setData(data: Any?) setData(data: Any?)")
-        list.forEach { title ->
-            val itemView = View.inflate(context, R.layout.layout_event_title, null)
-            val tvTitle = itemView.findViewById<TextView>(R.id.tvReportMessage)
-            tvTitle.text = title
-            val drawable = resources.getDrawable(R.drawable.shape_report_message_ring_blue)
-            drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
-            tvTitle.setCompoundDrawables(drawable, null, null, null)
-            llContent.addView(itemView)
-        }
     }
 
     override fun setData(data: Any?) {}
@@ -173,25 +94,6 @@ class MainFragment : BaseFragment<MainPresenter?>(), MainContract.View {
         mZhihuInfoService?.let {
             btnModule2.text = it.info.name
         }
-        mGankInfoService?.let {
-            btnModule2.text = it.info.name
-        }
-        mTestInfoService?.let {
-            btnModule3.text = it.info.name
-        }
-    }
-
-
-    fun showBatteryOptimizeTimeDialog(ctx: Context, selectKey: String, action: (item: ItemSelectDialog.DefaultItemSelect) -> Unit) {
-        val items = mutableListOf(
-                ItemSelectDialog.DefaultItemSelect("10$", "10"),
-                ItemSelectDialog.DefaultItemSelect("15$111", "15"),
-                ItemSelectDialog.DefaultItemSelect("20$111", "20"),
-                ItemSelectDialog.DefaultItemSelect("30$111", "30"),
-                ItemSelectDialog.DefaultItemSelect("60$111", "60")
-        )
-        val dialog = ItemSelectDialog(ctx, "一个title", items, selectKey, action)
-        dialog.show()
     }
 
     companion object {
--- app/src/main/java/com/gas/app/ui/fragment/mine/MineFragment.kt
+++ app/src/main/java/com/gas/app/ui/fragment/mine/MineFragment.kt
@@ -1,21 +1,32 @@
 package com.gas.app.ui.fragment.mine
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Process
+import android.text.Html
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.base.baseui.dialog.ItemSelectDialog
 import com.base.lib.base.LazyLoadFragment
 import com.base.lib.di.component.AppComponent
 import com.gas.app.R
 import com.gas.app.ui.fragment.mine.di.DaggerMineComponent
 import com.gas.app.ui.fragment.mine.mvp.MineContract
 import com.gas.app.ui.fragment.mine.mvp.MinePresenter
+import com.gas.app.utils.AppModuleUtil
 import com.gas.app.utils.RemoteActivity
 import com.gas.app.utils.getProcessName
 import com.lib.commonsdk.extension.app.debug
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_mine.*
 
 
@@ -23,6 +34,7 @@ class MineFragment : LazyLoadFragment<MinePresenter>(), MineContract.View {
 
 
 
+    private var btnMine1:View?= null
 
 
     override fun setupFragmentComponent(appComponent: AppComponent) {
@@ -35,13 +47,27 @@ class MineFragment : LazyLoadFragment<MinePresenter>(), MineContract.View {
 
     override fun initData(savedInstanceState: Bundle?) {
 
-        btnMine1.setOnClickListener {
+        val textV =  view?.findViewById<TextView>(R.id.showCode)
+
+        view?.findViewById<View>(R.id.btnMine1)?.setOnClickListener {
             debug(getProcessName(requireActivity().application, Process.myPid()))
         }
-        btnMine2.setOnClickListener {
-            startActivity(Intent(activity,RemoteActivity::class.java))
+        view?.findViewById<View>(R.id.btnMine2)?.setOnClickListener {
+            showBatteryOptimizeTimeDialog(context as Activity, "10") { item ->
+                debug(item.key)
+            }
         }
-
+        view?.findViewById<View>(R.id.btnMine3)?.setOnClickListener {
+            val str1 = getString(R.string.little_cloud_data_size, AppModuleUtil.formatMemorySizeB(53687091200), AppModuleUtil.formatMemorySizeB(12345678))
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                textV?.text = Html.fromHtml(str1, Html.FROM_HTML_MODE_LEGACY);
+            } else {
+                textV?.text = Html.fromHtml(str1);
+            }
+        }
+        view?.findViewById<View>(R.id.btnMine4)?.setOnClickListener {
+            initEventTitle()
+        }
     }
 
     override fun setData(data: Any?) {}
@@ -52,6 +78,54 @@ class MineFragment : LazyLoadFragment<MinePresenter>(), MineContract.View {
         get() = requireContext()
 
     override fun showMessage(message: String) {}
+
+    private fun showBatteryOptimizeTimeDialog(ctx: Context, selectKey: String, action: (item: ItemSelectDialog.DefaultItemSelect) -> Unit) {
+        val items = mutableListOf(
+                ItemSelectDialog.DefaultItemSelect("10$", "10"),
+                ItemSelectDialog.DefaultItemSelect("15$111", "15"),
+                ItemSelectDialog.DefaultItemSelect("20$111", "20"),
+                ItemSelectDialog.DefaultItemSelect("30$111", "30"),
+                ItemSelectDialog.DefaultItemSelect("60$111", "60")
+        )
+        val dialog = ItemSelectDialog(ctx, "一个title", items, selectKey, action)
+        dialog.show()
+    }
+
+
+
+
+    private fun textFormatStyle(num: Int): SpannableStringBuilder {
+        return if (num > 0) {
+            val startStr = num.toString()
+            val endFix = "条"
+            val str = "$startStr$endFix"
+            SpannableStringBuilder(str).apply {
+                setSpan(ForegroundColorSpan(Color.parseColor("#2B2D2F")), 0, startStr.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                setSpan(AbsoluteSizeSpan(30, true), 0, startStr.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                setSpan(ForegroundColorSpan(Color.parseColor("#7A7E8E")), startStr.length, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                setSpan(AbsoluteSizeSpan(9, true), startStr.length, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+        } else {
+            SpannableStringBuilder("暂无消息")
+        }
+    }
+
+    private fun initEventTitle() {
+        llContent.removeAllViews()
+        val list = listOf("第一条Preconditions.checkNotNull(message) setData(data: Any?) setData(data: Any?)",
+                " 第二条 ArmsUtils.snackbarText(message) Preconditions.checkNotNull(intent)",
+                "第三条 ArmsUtils.startActivity(intent) Preconditions.checkNotNull(intent)",
+                "第四条 Preconditions.checkNotNull(message) setData(data: Any?) setData(data: Any?)")
+        list.forEach { title ->
+            val itemView = View.inflate(context, R.layout.layout_event_title, null)
+            val tvTitle = itemView.findViewById<TextView>(R.id.tvReportMessage)
+            tvTitle.text = title
+            val drawable = resources.getDrawable(R.drawable.shape_report_message_ring_blue)
+            drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+            tvTitle.setCompoundDrawables(drawable, null, null, null)
+            llContent.addView(itemView)
+        }
+    }
 
     companion object {
         const val TAG = "MineFragment"
